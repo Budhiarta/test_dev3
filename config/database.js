27b5c7220d6da748f2c9f08d47e6f54405c15cae
Budhiarta <budhisuputra13@gmail.com>
2022-12-12 20:17:18 +0800

    add env

--- config/database.js
+++ config/database.js
@@ -1,36 +1,46 @@
-/**
- * @file Manages database connection configuration.
- * @author Fikri Rahmat Nurhidayat
- */
+const Sequelize = require("sequelize");
 
 /** Destruct environment variable to get database configuration */
 const {
   DB_USERNAME = "postgres",
-  DB_PASSWORD = "020711",
-  DB_HOST = "127.0.0.1",
-  DB_NAME = "db_Final",
+  DB_PASSWORD = "5TOhpYbeOiOIGUNfUw7c",
+  DB_HOST = "containers-us-west-110.railway.app",
+  DB_NAME = "railway",
+  DB_PORT = "5459",
+  DB_URL = "postgresql://postgres:5TOhpYbeOiOIGUNfUw7c@containers-us-west-110.railway.app:5459/railway"
 } = process.env;
+
+const db = new Sequelize(DB_URL, {
+  define: {
+    timestamps: false
+  }
+})
+
 
 module.exports = {
   development: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
-    database: `${DB_NAME}`,
+    database: DB_NAME,
     host: DB_HOST,
+    port: DB_PORT,
     dialect: "postgres",
   },
   test: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_test`,
+    database: DB_NAME,
     host: DB_HOST,
+    port: DB_PORT,
     dialect: "postgres",
   },
   production: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_production`,
+    database: DB_NAME,
     host: DB_HOST,
+    port: DB_PORT,
     dialect: "postgres",
   },
+  db
 };
