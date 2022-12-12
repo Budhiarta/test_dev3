9fc3b83f006c6c668bf861fd2b6ab3484c3f7910
Budhiarta <budhisuputra13@gmail.com>
2022-12-12 20:23:09 +0800

    update env, dbconfig

--- config/database.js
+++ config/database.js
@@ -3,11 +3,11 @@ const Sequelize = require("sequelize");
 /** Destruct environment variable to get database configuration */
 const {
   DB_USERNAME = "postgres",
-  DB_PASSWORD = "5TOhpYbeOiOIGUNfUw7c",
-  DB_HOST = "containers-us-west-110.railway.app",
+  DB_PASSWORD = "BsP0JunkZgu0Ll0D3ceV",
+  DB_HOST = "containers-us-west-169.railway.app",
   DB_NAME = "railway",
-  DB_PORT = "5459",
-  DB_URL = "postgresql://postgres:5TOhpYbeOiOIGUNfUw7c@containers-us-west-110.railway.app:5459/railway"
+  DB_PORT = "6964",
+  DB_URL = "postgresql://postgres:BsP0JunkZgu0Ll0D3ceV@containers-us-west-169.railway.app:6964/railway"
 } = process.env;
 
 const db = new Sequelize(DB_URL, {
